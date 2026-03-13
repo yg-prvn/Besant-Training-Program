5b45b095951828b5a87be189aa1bbc5d1cb9962c
praveen_kpm01 <praveenkpm1103@gmail.com>
2026-03-13 18:13:20 +0530

    March 13

---
+++ Python/March12.py
@@ -0,0 +1,24 @@
+# Tuples
+
+tup = (1,2,3,1,2,3,4,1,2,3)
+print(len(tup))
+
+
+l = [1,2,3,4,5,6,1,2,3]
+l.remove(1)
+print(l)
+
+print(tup)
+print(len(tup))
+print(max(tup))
+print(min(tup))
+print(tup.count(2))
+
+
+for i in tup:
+    print(i)
+
+
+# Sets
+s = {1,2,3,4,5,6,1,2,3,4,5,6}
+print(s)
